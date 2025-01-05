ae07b3e21f72dc1fdab93c8686c13919c3d19896
Abdul Ahad <156930361+AbdulAhad50@users.noreply.github.com>
2025-01-05 15:00:36 +0500

    index5

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+// The function 'cn' takes any number of class values and merges them using clsx and twMerge
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(...inputs)) // Ensure the spread operator is used correctly with clsx
 }
