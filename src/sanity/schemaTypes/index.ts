7c57815851d97c836b7a1f9f6d8d211c4169557b
Abdul Ahad <156930361+AbdulAhad50@users.noreply.github.com>
2025-01-05 14:51:26 +0500

    index3

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,9 +1,5 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import blog from './blog' // Make sure './blog' is correctly imported
+import blog from './blog'
 
-// Correctly typing the schema object
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [
-    blog, // blog should be a valid schema definition
-  ],
+export const schema = {
+  types: [blog],
 }
